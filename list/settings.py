f065317dbd627ca5830b5e8a4b72cb6525353d82
RehanaKhan01 <rehanakhan151@gmail.com>
2020-08-13 20:20:16 +0200

    DB3

--- list/settings.py
+++ list/settings.py
@@ -78,9 +78,11 @@ WSGI_APPLICATION = 'list.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
 
 
 # Password validation
