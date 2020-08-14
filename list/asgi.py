32be15de5788fc3792052457960fa0c786049f7a
RehanaKhan01 <rehanakhan151@gmail.com>
2020-08-14 02:01:16 +0200

    neu.2.3

--- list/asgi.py
+++ list/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Projekt2 project.
+ASGI config for list project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Projekt2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'list.settings')
 
 application = get_asgi_application()
